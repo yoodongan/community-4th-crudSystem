553ecca459549021aa1c261032493dcc7e7ac3cd
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-04 11:03:04 +0900

    setting

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,18 @@
+package com.ll.exam;
+
+import com.ll.exam.article.controller.ArticleController;
+import com.ll.exam.article.repository.ArticleRepository;
+import com.ll.exam.article.service.ArticleService;
+import com.ll.exam.home.controller.HomeController;
+import com.ll.exam.util.Ut;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+}
+
